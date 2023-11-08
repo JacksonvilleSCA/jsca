b0d4601ed3bb87e7f127b1748dc0b09d74a3b98f
Juan Marrugo <71800272+juanmarr@users.noreply.github.com>
2023-11-07 19:52:08 -0500

    updates

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import CreateList from './components/CreateList';
-import Itenerary from './components/Itenerary';
+import Itenerary from './components/Itinerary';
 import Nav from './components/Nav'
 
 export default function Home() {
