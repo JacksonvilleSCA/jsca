b2a2eaa5ddac6acb90f4aafb54e39015a5a45dce
Jean-Kerby <jeankerby4@gmail.com>
2023-11-07 21:26:54 -0500

    more comment

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,53 +1,53 @@
-"use client";
-import { Cagliostro } from "next/font/google"
-import Button from "../components/Button/Button"
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { useState } from "react";
+// "use client";
+// import { Cagliostro } from "next/font/google"
+// import Button from "../components/Button/Button"
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+// import { useState } from "react";
 
-const  Dashboard = () => {
-  const router = useRouter();
+// const  Dashboard = () => {
+//   const router = useRouter();
 
-  const [currentUser, setCurrentUser] = useState(null);
+//   const [currentUser, setCurrentUser] = useState(null);
 
-  const handleLogout = () => {
-    localStorage.removeItem("currentUser")
-    router.push("/")
-  }
+//   const handleLogout = () => {
+//     localStorage.removeItem("currentUser")
+//     router.push("/")
+//   }
 
-  useEffect(() => {
-    const user = localStorage.getItem("currentUser");
-    const userData = user && JSON.parse(user)
+//   useEffect(() => {
+//     const user = localStorage.getItem("currentUser");
+//     const userData = user && JSON.parse(user)
 
-    if(!userData){
-      router.push("/login?error=You are not logged in")
-    }else{
-      setCurrentUser(userData)
-    }
-  }, [router])
-
-  
-  
-
-
-    return currentUser ?(
-      <div>
-      <h1>JSCA Dashboard</h1>
-      <p>Welcome User: {currentUser.user.email}</p>
-      <br></br>
-      <Button url="/accountcreate" text ="Create Account"></Button>
-      <br></br>
-      <Button url="http://localhost:3000/" text ="Home"></Button>
-      <br></br>
-      <Button url="/accountmanage" text="Manage Account"></Button>
-      <br></br>
-      <button onClick={handleLogout}>Logout</button>
-
+//     if(!userData){
+//       router.push("/login?error=You are not logged in")
+//     }else{
+//       setCurrentUser(userData)
+//     }
+//   }, [router])
 
   
-      </div>
-    ) : null
-  }
+  
 
-  export default Dashboard;
+
+//     return currentUser ?(
+//       <div>
+//       <h1>JSCA Dashboard</h1>
+//       <p>Welcome User: {currentUser.user.email}</p>
+//       <br></br>
+//       <Button url="/accountcreate" text ="Create Account"></Button>
+//       <br></br>
+//       <Button url="http://localhost:3000/" text ="Home"></Button>
+//       <br></br>
+//       <Button url="/accountmanage" text="Manage Account"></Button>
+//       <br></br>
+//       <button onClick={handleLogout}>Logout</button>
+
+
+  
+//       </div>
+//     ) : null
+//   }
+
+//   export default Dashboard;
   
