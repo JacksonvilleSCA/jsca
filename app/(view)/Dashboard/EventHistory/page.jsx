e1d562ec314a1ae47e29fc1ee3bbba9546dd2866
Jean-Kerby <jeankerby4@gmail.com>
2023-12-07 19:03:43 -0500

    slight changes

--- app/(view)/Dashboard/EventHistory/page.jsx
+++ app/(view)/Dashboard/EventHistory/page.jsx
@@ -24,7 +24,7 @@ export default function Page() {
       }
     };
     fetchData();
-  }, []);
+  }, [eventInfomration]);
 
   return (
     <>
--- app/api/db/dbConnection.js
+++ app/api/db/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI3,{
+        await mongoose.connect(process.env.MONGO_DB_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true
 
