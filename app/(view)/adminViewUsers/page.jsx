b367e696ff8687c38cecf3abe9aa74636e329e57
RoflCopter223 <stashoe2568@gmail.com>
2024-01-03 12:23:01 -0500

    Users can be displayed. Next is fixing user accounts then modification.

--- app/(view)/adminViewUsers/page.jsx
+++ app/(view)/adminViewUsers/page.jsx
@@ -5,13 +5,32 @@ import { useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { useSearchParams } from "next/navigation"
 import Link from "next/link"
-import {accInfo} from "../../api/routes/accountInfo"
+import { getAllUsers } from "@/app/api/routes/users"
 
 const AdminUV = () => {
 
   const router = useRouter();
   const searchParams = useSearchParams();
   var search = searchParams.get('myID')
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const usersData = await getAllUsers();
+        setUsers(usersData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setLoading(false);
+      }
+    }
+    fetchUsers();
+  }, []);
+
+  
+
 
 
   function back(){
@@ -27,24 +46,22 @@ const AdminUV = () => {
     return (
       <div>
       <h1>ADMIN OVERVIEW</h1>
-      <h2>{search}</h2>
-      <div>
+      {/* Your other JSX */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
         <div>
           <p>User list goes here.</p>
-       
+          <ul>
+            {users.map((user, index) => (
+              <li key={index}>
+                Name: {user.username}, Email: {user.email}
+              </li>
+            ))}
+          </ul>
         </div>
-        <div>
-        <button onClick={back}>Return</button>
-        </div>
-      </div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-
-      </div>
-
-      
+      )}
+    </div>
     )
   }
 
---
+++ app/api/routes/users.js
@@ -0,0 +1,28 @@
+'use server'
+import mongoose from "mongoose";
+import Create from "../schema/Create";
+import connect from "../db/dbConnection";
+import React from "react";
+
+
+export async function getAllUsers() {
+  try {
+      const users = await Create.find({}); // Fetch all users from the 'Create' collection
+      
+      // Optional: Transform the user data to fit your needs
+      const formattedUsers = users.map(user => ({
+          email: user.email,
+          username: user.username,
+          password: user.password,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          phonenumber: user.phonenumber
+      }));
+      console.log("users are:")
+      console.log(formattedUsers);
+      return formattedUsers;
+  } catch (error) {
+      console.error('Error fetching users:', error);
+      return [];
+  }
+}
