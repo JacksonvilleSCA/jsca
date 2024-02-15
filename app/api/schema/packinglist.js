589fd09f113e1d6b7367256a95893f0b0fce7f83
RoflCopter223 <stashoe258@gmail.com>
2024-02-15 16:56:30 -0500

    testing bug fixes

--- app/api/schema/packinglist.js
+++ app/api/schema/packinglist.js
@@ -1,18 +1,18 @@
-//import mongoose from "mongoose";
-//import { Schema } from "mongoose";
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
 
-//const packingSchema = new Schema(
-    //{
-        // eventId: {
-        //     type: Schema.Types.ObjectId,
-        //     ref:'event',
-        //     required: false
-        // },
-        //items: [{
-            //type: String
-          //}]
-    //});
+const packingSchema = new Schema(
+    {
+         eventId: {
+             type: Schema.Types.ObjectId,
+             ref:'event',
+             required: false
+         },
+        items: [{
+            type: String
+          }]
+    });
 
-//const Packlist = mongoose.models.Packlist ||mongoose.model("Packlist", packingSchema);
+const Packlist = mongoose.models.Packlist ||mongoose.model("Packlist", packingSchema);
 
-//export default Packlist;
+export default Packlist;
