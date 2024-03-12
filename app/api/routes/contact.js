dfa15e5e7b14683305f3817c0bd3ad91a77142bc
RoflCopter223 <stashoe2568@gmail.com>
2024-03-11 21:18:43 -0400

    fixes

--- app/api/routes/contact.js
+++ app/api/routes/contact.js
@@ -1,23 +1,3 @@
-"use server";
-
-
-
-export async function contactTwo(data){
-        console.log(data + " +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        const username = process.env.NEXT_PUBLIC_EMAIL_USERNAME;
-        const password = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
-console.log(username + " " + password)
-//   return (
-
-//     <div> 
-        
-//     </div>
-//   )
-}
-
-
-
-
 'use server'
 import mongoose from "mongoose";
 import connect from "../db/dbConnection";
