ca9f5cb74382548f631c22b1ba9bbd48cd85ce07
Lizzzieee3 <golizzie@outlook.com>
2024-03-26 17:07:17 -0400

    stying !!!!!!

--- app/(view)/studentViewForm/page.jsx
+++ app/(view)/studentViewForm/page.jsx
@@ -31,120 +31,147 @@ const StudentViewForm = () => {
     fetchFormData();
   }, []);
   return (
-  <div> 
-    <NavTwo/> 
-    <div className="page-container">
-      <h1 style={{ textAlign: 'center' }}>Your Application </h1>
-      <div className='mb-3'> 
-          <a class="btn btn-primary me-2" href='/LoginDashboard' role="button">Back to Dashboard </a>
-      </div>
-       
-     
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <div className="card bg-primary text-white">
-              <div className="card-body">
-                {formData && (
-                 <div>
-                  <div class = "row"> 
-                    <div class = "col"> 
-                      <p>First Name: {formData.firstName}</p>
-                    </div> 
-                    <div class = "col"> 
-                      <p>Last Name: {formData.lastName}</p>
-                    </div> 
-                    <div class = "col"> 
-                      <p> Middle Name: {formData.middleName} </p> 
-                    </div> 
-                  </div> {/* first row */} 
-                  <div class = "row"> 
-                    <div class = "col"> 
-                      <p> Perferred Name: {formData.perferredName} </p>
-                    </div> 
-                    <div class = "col"> 
-                      <p>Student Phone Number:  {formData.studentPhone}</p>
-                    </div> 
-                  </div> {/* second row */} 
-                  <div class = "row"> 
-                    <div class = "col"> 
-                      <p> Sex: {formData.sex} </p>
-                    </div> 
-                    <div class = "col"> 
-                      <p> Date of Birth: {formData.dob.toLocaleDateString()} </p>
-                    </div> 
-                    <div class = "col"> 
-                      <p> Grade Level: {formData.grade} </p>
-                    </div> 
-                   </div> {/* third row */}  
-                   <div class = "row"> 
-                    <div class = "col"> 
-                      <p> School: {formData.school} </p>
+    <div> 
+      <NavTwo /> 
+        <h1 style={{ textAlign: 'center' }}>Your Application </h1>
+        <div className='mb-3'> 
+          <a class="btn btn-primary me-2 " href='/LoginDashboard' role="button">Back to Dashboard </a>
+        </div>
+
+        <div className="container">
+          <div className="row justify-content-center" >
+            <div className="col-md-10">
+            <div className="card bg-light" style={{ border: '6.5px solid #007bff' }}>
+                <div className="card-body">
+                  {formData && (
+                    <div>
+                     <div className="row mb-3">
+                        <div className="col">
+                          <p>First Name: {formData.firstName}</p>
+                         </div>
+                         <div className="col">
+                           <p>Last Name: {formData.lastName}</p>
+                         </div>
+                      </div> 
+                      <div className = "row mb-3">
+                        <div className="col">
+                          <p>Middle Name: {formData.middleName}</p> 
+                        </div>
+                        <div className="col">
+                          <p>Preferred Name: {formData.perferredName}</p>
+                        </div>
+                      </div> 
+
+                      <div className="row mb-3">
+                        <div className="col">
+                          <p>Student Phone Number:  {formData.studentPhone}</p>
+                        </div>
+                        <div className="col">
+                          <p>Sex: {formData.sex}</p>
+                        </div>
+                      </div>
+
+                      <div className="row mb-3">
+                       <div className="col">
+                        <p>Date of Birth: {new Date(new Date(formData.dob).setDate(new Date(formData.dob).getDate() + 1)).toLocaleDateString()}</p>
+                      </div>
+
+                        <div className="col">
+                          <p>Grade Level: {formData.grade}</p>
+                        </div>
+                      </div>
+
+                      <div className="row mb-3">
+                        <div className="col">
+                          <p>School: {formData.school}</p>
+                        </div>
+                        <div className="col">
+                          <p>Have a passport: {formData.passport}</p>
+                        </div>
+                      </div>
+
+                      <div className="row mb-3">
+                        <div className="col">
+                          <p>Passport Expiration Date: {new Date(new Date(formData.passportexp).setDate(new Date(formData.passportexp).getDate() + 1)).toLocaleDateString()}</p>
+                        </div>
+                        <div className="col">
+                          <p>Passport Country: {formData.passportCount}</p>
+                        </div>
+                      </div>
+
+                      <div className="row mb-3">
+                        <div className="col">
+                          <p>Allergies: {formData.allergies}</p>
+                        </div>
+                      </div>
+
+                     {/*} <h4 style={{ textAlign: 'right-centered' }}>Section Two</h4> */} 
+
+                      <div className="row mb-3">
+                        <div className="col">
+                          <p>Parent Name: {formData.parentname}</p>
+                        </div>
+                        <div className="col">
+                          <p>Second Parent Name: {formData.parenttwoname}</p>
+                        </div>
+                      </div>
+
+                      <div className="row mb-3">
+                        <div className="col">
+                          <p>Street: {formData.street}</p>
+                        </div>
+                        <div className="col">
+                          <p>Zip: {formData.zip}</p>
+                        </div>
+                      </div>
+                      <div className = "row mb-3"> 
+                        <div className="col">
+                          <p>City: {formData.city}</p>
+                        </div>
+                        <div className="col">
+                          <p>State: {formData.state}</p>
+                        </div>
+                      </div>
+
+                      <div className="row mb-3">
+                        <div className="col">
+                          <p>First Parent's Phone Number: {formData.parentphone}</p>
+                        </div>
+                        <div className="col">
+                          <p>Second Parent's Phone Number: {formData.parenttwophone}</p>
+                        </div>
+                      </div> 
+                      <div className="row mb-3">
+                        <div className="col">
+                          <p>Email: {formData.parentemail}</p>
+                        </div>
+                        <div className="col">
+                          <p>Parent signed on: {new Date(new Date(formData.signdate).setDate(new Date(formData.signdate).getDate() + 1)).toLocaleDateString()}</p>
+                        </div>
+                      </div>
+                      <div className="row mb-3">
+                        <div className="col">
+                          <p>Host Family and/or Travel: {formData.host}</p>
+                        </div>
+                        <div className="col">
+                          <p>Willing to host a family? {formData.hostfam}</p>
+                        </div>
+                      </div>
+                      <p> Essay: </p>
+                      <div className="container" style={{ border: '3px solid black' }}>
+                           <div dangerouslySetInnerHTML={{ __html: formData.essay }} /> 
+                      </div> 
+                        
+                     
                     </div>
-                    </div> {/* fourth row */} 
-
-                    {/* add Programs of Interest */}
-                    <div class = "row"> 
-                      <div class = "col"> 
-                        <p> Have a passport: {formData.passport} </p>
-                      </div> 
-                      <div class = "col"> 
-                       <p> Passport Expiration Date: {formData.passportexp.toLocaleDateString()}</p>
-                      </div> 
-                      <div class = "col"> 
-                       <p> Passport Country: {formData.passportCount} </p>
-                      </div> 
-                    </div> {/* fifth row */} 
-                    <p> Allgeries: {formData.allergies} </p>
-
-                    <h4 style={{ textAlign: 'center' }}> SECTION TWO </h4>
-                    <div class = "row"> 
-                      <div class = "col"> 
-                        <p> Parent Name: {formData.parentname} </p>
-                      </div> 
-                      <div class = "col"> 
-                        <p> Second Parent Name: {formData.parenttwoname} </p>
-                      </div> 
-                    </div> {/* row */} 
-                    <p> Street : {formData.street} </p>
-                    <div class = "row"> 
-                      <div class = "col"> 
-                        <p> City: {formData.city} </p>
-                      </div> 
-                      <div class = "col"> 
-                        <p> State: {formData.state} </p>
-                      </div> 
-                      <div class = "col"> 
-                        <p> Zip: {formData.zip} </p>
-                      </div> 
-                    </div> {/* row */} 
-                    <p> First Parents Phone Number: {formData.parentphone} </p>
-                    <p> Second Parents Phone Number: {formData.parenttwophone} </p>
-                    <p> Email: {formData.parentemail} </p>
-                    <p> Parent signed on: {formData.signdate.toLocaleDateString()}</p>
-                    <div class = "row"> 
-                      <div class = "col"> 
-                        <p> Host Family and/or Travel: {formData.host}  </p>
-                      </div> 
-                      <div class = "col"> 
-                       <p> Willing to host a family? {formData.hostfam} </p>
-                      </div> 
-                    </div> {/* row */}
-                    <p> Essay: <div dangerouslySetInnerHTML={{ __html: formData.essay }} /> </p>
-
-                    <p> </p>
-                    
-                 
-                </div>
-                )}
-             </div> 
+                  )}
+                </div> 
+              </div> 
             </div> 
-           </div> 
+          </div> 
         </div> 
-        
-      </div> 
+     
     </div>
-   </div> 
   );
 };
 
